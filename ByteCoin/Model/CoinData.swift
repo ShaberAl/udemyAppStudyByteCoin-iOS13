e7ee25c91a14c76c7290e041c557f5bcb892327c
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-10-25 20:11:12 +0500

    Ready

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,11 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by MacOS on 25.10.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+struct CoinData: Decodable {
+    let rate: Double
+}
